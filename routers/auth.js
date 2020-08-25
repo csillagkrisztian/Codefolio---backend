7e6bdf7c9eb3086a1702e8539290d4f7f037ea6b
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-08-25 15:23:56 +0200

    added projects to the /me and the /login endpoints

--- routers/auth.js
+++ routers/auth.js
@@ -3,6 +3,7 @@ const { Router } = require("express");
 const { toJWT } = require("../auth/jwt");
 const authMiddleware = require("../auth/middleware");
 const User = require("../models/").user;
+const Project = require("../models").project;
 const { SALT_ROUNDS } = require("../config/constants");
 
 const router = new Router();
@@ -17,7 +18,7 @@ router.post("/login", async (req, res, next) => {
         .send({ message: "Please provide both email and password" });
     }
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { email }, include: [Project] });
 
     if (!user || !bcrypt.compareSync(password, user.password)) {
       return res.status(400).send({
@@ -78,8 +79,11 @@ router.post("/signup", async (req, res) => {
 // - checking if a token is (still) valid
 router.get("/me", authMiddleware, async (req, res) => {
   // don't send back the password hash
-  delete req.user.dataValues["password"];
-  res.status(200).send({ ...req.user.dataValues });
+
+  const { email } = req.user;
+  const user = await User.findOne({ where: { email }, include: [Project] });
+  delete user.dataValues["password"];
+  res.status(200).send({ ...user.dataValues });
 });
 
 module.exports = router;
