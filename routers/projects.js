79c23505c9e623eb7640a272d8ecfcc97efe1ba5
Atieh Hamidi <hamidiatiye@gmail.com>
2020-08-25 17:58:27 +0200

    added /project/:id/comment for adding new comment

--- routers/projects.js
+++ routers/projects.js
@@ -97,4 +97,20 @@ router.get("/tags", async (req, res) => {
   const tags = await Tag.findAll();
   return res.status(201).send(tags);
 });
+
+router.post("/projects/:id/comment", authMiddleware, async (req, res) => {
+  const { comment } = req.body;
+  const { id } = req.params;
+
+  try {
+    const newComment = await Comment.create({
+      comment,
+      projectId: id,
+      userId: req.user.id,
+    });
+    return res.status(201).send(newComment);
+  } catch (error) {
+    return res.status(400).send({ message: "There is a problem", error });
+  }
+});
 module.exports = router;
