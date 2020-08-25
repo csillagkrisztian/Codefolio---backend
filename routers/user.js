fb440d23349ddf54bb1969fd17e574897fbf12f0
Atieh Hamidi <hamidiatiye@gmail.com>
2020-08-25 11:47:09 +0200

    added endpoint projects/id for getting one project include their comments likes and author(user)

--- routers/user.js
+++ routers/user.js
@@ -5,6 +5,7 @@ const User = require("../models/").user;
 const Project = require("../models/").project;
 const Comment = require("../models").comment;
 const Like = require("../models").like;
+const Resource = require("../models").resource;
 
 const router = new Router();
 
@@ -29,7 +30,24 @@ router.get("/homepage", async (req, res) => {
   const projects = await Project.findAll({
     include: [Comment, Like],
   });
-  return res.status(201).send(projects);
+  try {
+    return res.status(201).send(projects);
+  } catch (error) {
+    return res.status(400).send({ message: "There is a problem" });
+  }
 });
 
+router.get("/projects/:id", async (req, res) => {
+  const project = await Project.findByPk(req.params.id, {
+    include: [Resource, Comment, Like],
+  });
+  const userId = project.userId;
+  console.log("find userId", userId);
+  const user = await User.findByPk(userId);
+  try {
+    return res.status(201).send({ project, user });
+  } catch (error) {
+    return res.status(400).send({ message: "There is a problem" });
+  }
+});
 module.exports = router;
